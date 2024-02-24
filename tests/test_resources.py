34fef6aaf787c0054b817179a7e7ff0a5b5bc7df
Risto Korhonen <risto.korhonen@kapsi.fi>
2024-02-24 13:36:16 +0200

    Make tests run and first tests to pass

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -17,28 +17,31 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 
 
 @pytest.fixture
-def app():
+def client():
     db_fd, db_fname = tempfile.mkstemp()
     config = {
         "SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname,
         "TESTING": True
     }
-    
+
     app = create_app(config)
-    
+
     with app.app_context():
         db.create_all()
-        
-    yield app
-    
+
+    yield app.test_client()
+
     os.close(db_fd)
     os.unlink(db_fname)
 
-def test_create_sensor(app):
-    with app.app_context():
-        sensor = Sensor(
-            name="test-sensor-1",
-        )
-        db.session.add(sensor)
-        db.session.commit()
-        assert Sensor.query.count() == 1
+def test_hello_world(client):
+    # Simple test just to make sure that the tests run
+    pass
+
+class TestLocationResource(object):
+
+    RESOURCE_URL = "/api/locations/"
+
+    def test_get(self, client):
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
