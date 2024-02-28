30606b7a7787ec71dbea09bfb6a445a436ec859e
Risto Korhonen <risto.korhonen@student.oulu.fi>
2024-02-28 14:49:19 +0200

    Add PUT method to SensorItem

--- mokkiwahti/resources/sensor.py
+++ mokkiwahti/resources/sensor.py
@@ -32,16 +32,13 @@ class SensorCollection(Resource):
 
             db.session.add(sensor)
             db.session.commit()
-        except KeyError:
-            raise BadRequest
         except IntegrityError:
             raise Conflict(
                 description=f"Sensor with name: {sensor.name} already found"
             )
         
         return Response(status=201, headers={
-            #"Location": url_for("api.sensoritem", sensor=sensor.name)
-            "Location": "Location in progress @TODO"
+            "Location": url_for("api.sensoritem", sensor=sensor)
         })
 
 class SensorItem(Resource):
@@ -49,8 +46,21 @@ class SensorItem(Resource):
     def get(self, sensor):
         return Response(json.dumps(sensor.serialize()), 200, mimetype='application/json')
 
-    def put(self):
-        pass
+    def put(self, sensor):
+        if not request.json:
+            raise UnsupportedMediaType
+        
+        try:
+            validate(request.json, Sensor.get_schema())
+        except ValidationError as e:
+            raise BadRequest(description=str(e))
+        
+        sensor.deserialize(request.json)
+        db.session.commit()
+
+        return Response(status=200, headers={
+            "Location": "Location in progress @TODO"
+        })
 
     def delete(self, sensor): 
         # @TODO some error handling needed? Sensor is already validated by SensorConverter
