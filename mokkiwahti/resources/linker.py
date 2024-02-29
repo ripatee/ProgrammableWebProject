abb8edd9fd7c65cb3b2f9fb47e74fc2fab4fd43d
Risto Korhonen <risto.korhonen@student.oulu.fi>
2024-02-29 12:19:35 +0200

    Add Sensor to Location linker resource

---
+++ mokkiwahti/resources/linker.py
@@ -0,0 +1,20 @@
+import json
+from flask import request, Response, url_for
+from flask_restful import Resource
+from jsonschema import validate, ValidationError
+from mokkiwahti.db_models import Location, Sensor
+from mokkiwahti import db
+from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import Conflict, BadRequest, UnsupportedMediaType
+
+class LocationSensorLinker(Resource):
+
+    def put(self, sensor, location):
+        location.sensors.append(sensor)
+        db.session.commit()
+        return Response(status=200)
+    
+    def delete(self, sensor, location):
+        location.sensors.remove(sensor)
+        db.session.commit()
+        return Response(status=200)
