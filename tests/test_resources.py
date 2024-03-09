f2f60aed63b948204046dfa2e5e6cc1ca53a47ff
jeremiask-rkk <joona.korkko@student.oulu.fi>
2024-03-09 14:50:09 +0200

    Create classes and add tests for sensor, measurements, locations

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -44,6 +44,7 @@ def _get_sensor_configuration(interval = 900, threshold_min = 15.0, threshold_ma
 
 def _populate_db():
     for i in range(1, 4):
+        # create 3 instances per class
         s = Sensor(name="testsensor-{}".format(i))
         l = Location(name="testlocation-{}".format(i))
         sc = SensorConfiguration(
@@ -56,6 +57,12 @@ def _populate_db():
             humidity = i,
             timestamp = datetime.now()
         )
+        # add relationships
+        s.sensor_configuration = sc
+        l.sensors.append(s)
+        l.measurements.append(m)
+        s.measurements.append(m)
+        # add to DB
         db.session.add(s)
         db.session.add(l)
         db.session.add(sc)
@@ -64,6 +71,10 @@ def _populate_db():
     db.session.commit()
 
 def _check_db():
+        """
+        Prints contents of DB
+        Debug purposes
+        """
         print("location count: ", Location.query.count())
         print("sensor count: ", Sensor.query.count())
         print("sensor configuration count: ", SensorConfiguration.query.count())
@@ -100,21 +111,26 @@ class TestLocationResource(object):
     def test_get(self, client):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
+        body = json.loads(resp.data)
+        for loc in body:
+            validate(loc, Location.get_schema())
+
+    def test_post(self, client):
+        data = {"name": "testlocation-100"}
+        resp = client.post(self.RESOURCE_URL, json=data)
+        assert resp.status_code == 201
+        resp = client.get(self.RESOURCE_URL)
+        body = json.loads(resp.data)
+        new_found = False #temp to check that new loc is added
+        for loc in body:
+            validate(loc, Location.get_schema())
+            if loc["name"] == "testlocation-100":
+                new_found = True
+        assert new_found
 
 class TestLinkerResource(object):
 
     RESOURCE_URL = "/api/locations/testlocation-1/link/sensors/testsensor-1/"
-
-    # no get method in resource
-    def test_get(self, client):
-        resp = client.get(self.RESOURCE_URL)
-        assert resp.status_code == 405
-
-    # no post method in resource
-    def test_post(self, client):
-        sensor_data = {"name": "testsensor-100"}
-        resp = client.post(self.RESOURCE_URL, json=sensor_data)
-        assert resp.status_code == 405
 
     # test put method
     def test_put(self, client):
@@ -151,3 +167,81 @@ class TestLinkerResource(object):
     def test_put_wo_location(self, client):
         resp = client.put("/api/locations/testlocation-100/link/sensors/testsensor-1/")
         assert resp.status_code == 404
+
+class TestSensorResource(object):
+
+    RESOURCE_URL = "/api/sensors/"
+
+    def test_get(self, client):
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        for sensor in body:
+            validate(sensor, Sensor.get_schema())
+
+    def test_post(self, client):
+        data = {"name": "testsensor-100"}
+        sc = SensorConfiguration(
+            interval = 900,
+            threshold_min = 15,
+            threshold_max = 25
+        )
+        data["sensor_configuration"] = sc.serialize()
+        resp = client.post(self.RESOURCE_URL, json=data)
+        assert resp.status_code == 201
+        resp = client.get(self.RESOURCE_URL)
+        body = json.loads(resp.data)
+        assert resp.status_code == 200
+        new_found = False #temp to check that new sensor is added
+        for sensor in body:
+            validate(sensor, Location.get_schema())
+            if sensor["name"] == "testsensor-100":
+                new_found = True
+        assert new_found
+        
+@pytest.mark.skip(reason="Not implemented")
+class TestGetAllMeasurementResource(object):
+    
+    RESOURCE_URL = "/api/measurements/"
+    
+    pytest.mark.skip(reason="Not implemented")
+    def test_get(self, client):
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        for meas in body:
+            validate(meas, Measurement.get_schema())
+
+class TestMeasurementsResource(object):
+    
+    SENSOR_RESOURCE_URL = "/api/sensors/testsensor-1/measurements/"
+    LOCATION_RESOURCE_URL = "/api/locations/testlocation-1/measurements/"
+
+    def test_get_by_sensor(self, client):
+        resp = client.get(self.SENSOR_RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert len(body) == 1
+        validate(body[0], Measurement.get_schema())
+
+    def test_get_by_location(self, client):
+        resp = client.get(self.LOCATION_RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert len(body) == 1
+        validate(body[0], Measurement.get_schema())
+
+    def test_post(self, client):
+        meas_test_obj =  Measurement(
+            temperature = 22.2,
+            humidity = 55.2,
+            timestamp = datetime.now()
+        )
+        resp = client.post(self.SENSOR_RESOURCE_URL, json=meas_test_obj.serialize())
+        assert resp.status_code == 201
+        resp = client.get(self.SENSOR_RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert len(body) == 2
+        for meas in body:
+            validate(meas, Measurement.get_schema())
